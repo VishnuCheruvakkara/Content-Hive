9520c757bb60d994ca3749cce3c8ece69975eaa0
vishnu cheruvakkara <vishnucheruvakkaraofficial@gmail.com>
2025-12-06 14:09:53 +0530

    Update : Added new table for blogset up

--- backend/apps/blog/models.py
+++ backend/apps/blog/models.py
@@ -1,4 +1,39 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User=get_user_model()
 
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=500)
+    content_html = models.TextField()
+    image = models.ImageField(upload_to="blog_images/", null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+class Comment(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+    text = models.TextField(max_length=500)
+    created_at = models.DateTimeField(default=timezone.now)
+    is_approved = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.user} - {self.blog.title}"
+
+class Like(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+    is_like = models.BooleanField(default=True)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        unique_together = ("blog", "user")
+
+    def __str__(self):
+        return f"{self.user} - {self.blog.title} - {'Like' if self.is_like else 'Unlike'}"
