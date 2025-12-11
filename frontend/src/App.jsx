062737e4a1358a67bba0ed71c15c3ad420052933
vishnu cheruvakkara <vishnucheruvakkaraofficial@gmail.com>
2025-12-11 16:06:49 +0530

    fix : added proper frontend path for navigation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
 import RouterConfig from './routes/RouterConfig';
-import { Toaster } from 'react-hot-toast';
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
 import Spinner from './components/ui/Spinner';
 import useCSRF from './hooks/useCSRF';
-import NavigationRegistrar from './services/navigation/NavigationRegistrar';
+import NavigationRegistrar from './services/Navigation/NavigationRegistrar';
 import CustomToaster from './components/ui/CustomToaster';
 
 function App() {
